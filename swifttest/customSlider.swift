ff682372a3dd8b3438fd418aa7bfdcbae264042e
Jeff Berube <jberube@oilforce.com>
2015-04-08 18:51:06 -0600

    First and only commit

---
+++ swifttest/customSlider.swift
@@ -0,0 +1,11 @@
+//
+//  customSlider.swift
+//  swifttest
+//
+//  Created by Jean-Francois Berube on 2015-04-08.
+//  Copyright (c) 2015 Nordiq Digital. All rights reserved.
+//
+
+import Foundation
+
+
